513dcab7bbf00b2c7330793119d48704c8f4024a
kamil3397 <kkgolkiper33@gmail.com>
2025-03-24 10:27:48 +0100

    fetching tasks endpoint

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -50,4 +50,17 @@ export class TaskController {
       return res.status(500).send({ message: 'Internal Server Error' });
     }
   }
+
+  async getTasks(req: Request, res:Response) {
+    try {
+      const tasks = await this.taskCollection.find({}).toArray();
+      if (!tasks) {
+        return res.status(404).send({ message: 'Tasks not found' });
+      }
+      return res.status(200).send(tasks);
+    } catch (err) {
+      return res.status(500).send({ message: 'Internal Server Error' });
+    }
+
+  }
 }
